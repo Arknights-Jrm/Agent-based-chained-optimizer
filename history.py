bfcefce2c9cda1131c2ee9cc8b9f2a096ad1769f
mon3tr <mon3tr@paramer.top>
2026-03-26 21:56:26 +0800

    添加模拟器接口和历史记录保存功能

--- history.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def save_history(history_params, history_perf, filename="optimization_history.json"):
-    data = {
-        "params": history_params,
-        "performance": history_perf
-    }
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2)
